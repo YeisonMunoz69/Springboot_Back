57a8efe2ece7f4b9de0616c04fd47e73d32a0ba6
Yeison <yjmunoz@unimayor.edu.co>
2024-09-29 11:43:11 -0500

    Proyecto virgen, recien creado

---
+++ src/main/java/com/example/springboot_supabase_crud/SpringbootSupabaseCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_supabase_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSupabaseCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSupabaseCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_supabase_crud/SpringbootSupabaseCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_supabase_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSupabaseCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
